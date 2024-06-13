df550f9e93a901febb35a6012e4d2d7fc89b13a8
tarunmalik330 <maliktarun2005@gmail.com>
2024-06-13 10:13:39 +0530

    adding meta data

---
+++ src/pages/_document.js
@@ -0,0 +1,45 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>galileo-sky</title>
+        {/* <!-- Primary Meta Tags --> */}
+        <title>galileo-sky</title>
+        <meta name="title" content="galileo-sky" />
+        <meta
+          name="description"
+          content="Galileo Sky, founded by industry experts, is redefining the gaming landscape. With a blend of groundbreaking technology and immersive entertainment, we create world-class gaming experiences and thriving digital economies that captivate and inspire"
+        />
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="galileo-sky" />
+        <meta
+          property="og:description"
+          content="Galileo Sky, founded by industry experts, is redefining the gaming landscape. With a blend of groundbreaking technology and immersive entertainment, we create world-class gaming experiences and thriving digital economies that captivate and inspire"
+        />
+        <meta property="og:image" content="https://i.ibb.co/DryS3Wg/meta.png" />
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="galileo-sky" />
+        <meta
+          property="twitter:description"
+          content="Galileo Sky, founded by industry experts, is redefining the gaming landscape. With a blend of groundbreaking technology and immersive entertainment, we create world-class gaming experiences and thriving digital economies that captivate and inspire"
+        />
+        <meta
+          property="twitter:image"
+          content="https://i.ibb.co/DryS3Wg/meta.png"
+        />
+        {/* <!-- Meta Tags Generated with https://metatags.io --> */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
